9b4a3e31959df6d65c470b0ecedfd334b1fb3ecd
unknown <jh138861@UKNML6038.renishaw.com>
2014-08-20 10:12:20 +0100

    m

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
-/*var server = require('http').createServer(app);
+var server = require('http').createServer(app);
 var socketio = require('socket.io').listen(server);
 var sys = require('sys')
 var exec = require('child_process').exec;
@@ -40,7 +40,7 @@ require('./routes')(app);
 server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
   console.log('The Mongodb url is: ', config.mongo.uri);
-});*/
+});
 
 // Expose app
 exports = module.exports = app;

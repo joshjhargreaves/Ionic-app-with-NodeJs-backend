488eba506bf35b19dbe2c7858794b9618fb0cf44
unknown <jh138861@UKNML6038.renishaw.com>
2014-08-20 11:32:53 +0100

    message

--- server/testHook.js
+++ server/testHook.js
@@ -1,8 +1,6 @@
 var gith = require('gith').create(); // ensure to match the port you entered in Github
 
-gith({
-  repo: 'hardgreaves/fullstack'
-}).on( 'all', function( payload ) {
+gith().on( 'all', function( payload ) {
   console.log( 'Post-receive happened!' );
 });
 

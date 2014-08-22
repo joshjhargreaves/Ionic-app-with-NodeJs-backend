92851bb3465aa4e6b5bf7011538448b6a8479bb0
Josh Hargreaves <joshyhargreaves@gmail.com>
2014-08-22 23:33:19 +0100

    m

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ require('./routes')(app);
 
 // Start server
 server.listen(config.port, config.ip, function () {
-	console.log("Route: %s", config.route);
+	console.log("Route: %s", config.root);
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
   console.log('The Mongodb url is: ', config.mongo.uri);
 });

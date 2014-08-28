76d440cd1cb03da1d45f100cece23e5636244900
unknown <jh138861@UKNML6038.renishaw.com>
2014-08-28 14:21:49 +0100

    m

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -44,7 +44,7 @@ var all = {
   facebook: {
     clientID:     process.env.FACEBOOK_ID || 'id',
     clientSecret: process.env.FACEBOOK_SECRET || 'secret',
-    callbackURL:  '/auth/facebook/callback'
+    callbackURL:  process.env.DOMAIN + '/auth/facebook/callback'
   },
 
   twitter: {

b4276d997f0c3e2d4278c4118b718972ce683a43
unknown <jh138861@UKNML6038.renishaw.com>
2014-08-28 13:02:07 +0100

    Changed route

--- server/routes.js
+++ server/routes.js
@@ -21,6 +21,6 @@ module.exports = function(app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/www/index.html');
+      res.sendfile(app.get('appPath') + '/index.html');
     });
 };
